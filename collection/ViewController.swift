f8bec17aa34db3838a513ded53c0bbf7a7a0e0b9
adhikarivijay05 <adhikari.vijay05@example.comgit config --global user.email adhikari.vijay05@example.com>
2017-08-29 14:03:46 +0530

    collectionview_animation_touch
    collectionview_animation_touch

--- collection/ViewController.swift
+++ collection/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var colView: UICollectionView!
+    
+    var imageArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCollectionViewCell
+        cell.img.image = UIImage.init(named: imageArray[indexPath.row])
+        
+        return cell
+    
+    }
+    @nonobjc func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: IndexPath) {
+        animateCell(cell: cell)
+    }
+    
+    func animateCell(cell: UICollectionViewCell) {
+        let animation = CABasicAnimation(keyPath: "cornerRadius")
+        animation.fromValue = 200
+        cell.layer.cornerRadius = 0
+        animation.toValue = 0
+        animation.duration = 1
+        cell.layer.add(animation, forKey: animation.keyPath)
+    }
+    
+    func animateCellAtIndexPath(indexPath: NSIndexPath) {
+        guard let cell = colView.cellForItem(at: indexPath as IndexPath) else { return }
+        animateCell(cell: cell)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        animateCellAtIndexPath(indexPath: indexPath as NSIndexPath)
+    }
+    
 }
 
---
+++ collection/myCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  myCollectionViewCell.swift
+//  collection
+//
+//  Created by Vijay Adhikari on 29/08/2017.
+//  Copyright © 2017 Vijay Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class myCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+}
